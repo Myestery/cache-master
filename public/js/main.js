e0264eafbe3c47216a3ca12a2742bdf16541f7ef
Johnpaul <polymenjohn1@gmail.com>
2023-10-28 12:59:50 +0100

    america

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,2 @@
 const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(React.createElement("h1", null, "Hello, judah"));
+root.render(React.createElement("h1", null, "Hello, america"));
